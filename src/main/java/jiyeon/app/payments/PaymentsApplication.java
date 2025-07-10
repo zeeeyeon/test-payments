9541835bd87b5a23b804668cf45bbc13c61e6448
zeeeyeon <zeeyeonleee@gmail.com>
2025-07-11 03:18:57 +0900

    feat : JPA 설정

--- src/main/java/jiyeon/app/payments/PaymentsApplication.java
+++ src/main/java/jiyeon/app/payments/PaymentsApplication.java
@@ -2,7 +2,11 @@ package jiyeon.app.payments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableJpaAuditing
 @SpringBootApplication
 public class PaymentsApplication {
 
